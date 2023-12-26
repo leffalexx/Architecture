4672e299dbfe16f0e0f24655f7519391a82f6fae
Aleksei <leffalexx@gmail.com>
2023-12-26 22:51:46 +0300

    ASP.NET Core added

---
+++ HW_7v2/SPA/SPA/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Err()
+        {
+            return View();
+        }
+    }
+}
---
+++ HW_7v2/SPA/SPA/Program.cs
@@ -0,0 +1,22 @@
+namespace SPA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Home}");
+
+            app.Run();
+        }
+    }
+}
